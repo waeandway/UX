961b085615432503369d26eb16549cda31062c4c
yun-n <jyk0321@likelion.org>
2020-11-11 13:04:07 +0900

    Array 객체

--- js/array.js
+++ js/array.js
@@ -1,3 +1,4 @@
+//배열 기초
 var plots = [5, 4, 3, 2, 1]; // 크기가 5인 배열 
 
 for(i=0; i<5; i++) {
@@ -27,7 +28,29 @@ document.write(week + "<br>")
 var n = [5, 4, 3, 2, 1];
 var sum = 0;
 
-for(i=0; i<5; i++) {
+/*for(i=0; i<5; i++) {
     sum = sum + n[i];
 }
-document.write("sum = " + sum + "<br>")
+document.write("sum = " + sum + "<br>");*/
+
+/***********************************************************************************/
+
+// Array 객체로 배열 생성
+var degree = new Array(); // new 키워드 + Array 객체, 빈 객체 생성
+degree[0] = 10;
+degree[1] = 11;
+degree[2] = 12;
+degree[3] = 13;
+degree[4] = 14;
+
+ for(i=0; i<degree.length;i++) {
+    sum += degree[i];
+ }
+
+document.write("평균 = " + sum/degree.length + "<br>");
+
+/***********************************************************************************/
+
+
+
+
---
+++ js/prac.js
@@ -0,0 +1,6 @@
+var color = new Array("빨", "주", "노", "초", "파", "남", "보");
+ 
+ for(i=0; i<color.length; i++) {
+     colors = color[i]
+     document.write(colors + "<br>")
+ }
