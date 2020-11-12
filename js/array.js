f97865d471675017d0d462f652d2babd2bdc05cc
yun-n <jyk0321@likelion.org>
2020-11-12 11:21:33 +0900

    Array 객체 메소드

--- js/array.js
+++ js/array.js
@@ -51,6 +51,27 @@ document.write("평균 = " + sum/degree.length + "<br>");
 
 /***********************************************************************************/
 
+// Array 객체의 메소드 
+function pr(msg, arr) {
+    document.write(msg + arr.toString() + "<br>");
+}
 
+var a = new Array("a" , "b", "c");
+var b = new Array("d");
+var c;
 
+c = a.concat(b); // concat 메소드 : c는 a와 b를 연결
+pr("c = ", c); // 함수 호출
+
+c = a.join("#"); // join 메소드 : 배열 a를 특정 문자열로 연결하여 하나의 문자열로 리턴
+pr("c = ", c);
+
+c = a.reverse(); // reverse 메소드 : a 역순
+pr("c = ", c);
+
+c = a.slice(0,1); // slice 메소드 : 배열 [0] <= x < [1] 을 의미
+pr("c = ", c);
+
+c = a.toString(); // toString 메소드 : 배열 a를 "," 로 연결하여 하나의 문자열로 리턴
+pr("c =", c);
 
